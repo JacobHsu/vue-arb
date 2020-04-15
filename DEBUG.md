4205b616c96aa7a7e3a4895f608a7c6a1499a0d9
jacobhsu <jacob.hsu.tw@gmail.com>
2020-04-15 11:34:44 +0800

    core-js module error #3678

---
+++ DEBUG.md
@@ -0,0 +1,11 @@
+# Debug
+
+## core-js
+
+These dependencies were not found:
+* core-js/modules/es.array.index-of 
+
+[core-js module error #3678](https://github.com/vuejs/vue-cli/issues/3678)
+babel.config.js
+
+`$ yarn add core-js --dev`
--- src/main.js
+++ src/main.js
@@ -2,11 +2,33 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
+
+i18n.locale = 'zh-CN'
+
+Vue.mixin({
+  methods: {
+    goTo: function (path) {
+      this.$router.push({ name: path })
+    }
+  }
+})
+
+const userLang = navigator.language || navigator.userLanguage
+localStorage.setItem('userLang', userLang)
+
+const defaultLang = userLang.indexOf('en') === -1 ? 'zh-CN' : 'en-US'
+const lang = localStorage.getItem('lang')
+
+if (!lang) {
+  localStorage.setItem('lang', defaultLang)
+} 
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
